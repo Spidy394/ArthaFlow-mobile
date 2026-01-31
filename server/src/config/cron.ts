eed58604170e122f7f3ca6ceebeea851307f80f6
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2026-01-31 21:27:36 +0530

    add a cron-job to avoid cold-start by render

---
+++ server/src/config/cron.ts
@@ -0,0 +1,14 @@
+import cron from "cron";
+import https from "https"
+
+// one get request every 14 min
+const job = new cron.CronJob("*/14 * * * *", () => {
+    https
+    .get(process.env.API_URL!, (res) => {
+        if(res.statusCode === 200) console.log("GET request sent successfully");
+        else console.log("GET request failed", res.statusCode);
+    })
+    .on("error", (e) => console.error("Error while sending request", e));
+});
+
+export default job;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,19 @@
 import express from "express";
 import rateLimiter from "./middleware/rateLimiter";
 import transactionRouter from "./route/transactionRoutes"
+import job from "./config/cron";
 
 const app = express();
 
 app.use(express.json());
 app.use(rateLimiter);
 
+if(process.env.NODE_ENV === "production") job.start();
+
 app.use("/api/transaction", transactionRouter)
 
 app.get("/health", (req, res) => {
-    res.send({
+    res.status(200).json({
         "status": "ok"
     });
 });
