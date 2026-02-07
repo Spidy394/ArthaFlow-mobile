5464b13b27e1a20a74abb4c1c8cd1589746e8a1a
Shubhodeep Mondal <mondalshubhodeep30924@gmail.com>
2026-02-07 18:20:35 +0530

    stop cron job

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import rateLimiter from "./middleware/rateLimiter";
 import transactionRouter from "./route/transactionRoutes"
-import job from "./config/cron";
+// import job from "./config/cron";
 
 const app = express();
 
 app.use(express.json());
 app.use(rateLimiter);
 
-if(process.env.NODE_ENV === "production") job.start();
+// if(process.env.NODE_ENV === "production") job.start();
 
 app.use("/api/transaction", transactionRouter)
 
